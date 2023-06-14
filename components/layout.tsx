1c1ed3c8b7e1ee9a3c33f42ff8780df25ef1c623
umaprasath <umaprasath.lru@gmail.com>
2023-06-14 10:24:45 -0500

    github integration added

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./header"
+import Footer from "./footer"
+import type { ReactNode } from "react"
+import React from "react"
+
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+
