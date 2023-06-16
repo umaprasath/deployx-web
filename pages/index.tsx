6cc313b848eda1225e47c9a4b29bdbaf1dcdd82f
umaprasath <umaprasath.lru@gmail.com>
2023-06-16 10:27:38 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function IndexPage() {
       <h1>NextAuth.js Example</h1>
       <p>
         This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+       
       </p>
     </Layout>
   )
